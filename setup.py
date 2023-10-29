907f65f4ebfdfb70daa460f89082a2e445d0996a
Alec Ostrander <alec.ostrander@growersedge.com>
2023-10-28 22:09:03 -0500

    build/publish update; cleanup

--- setup.py
+++ setup.py
@@ -5,24 +5,24 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cleantimer",
-    version="0.0.1",
+    version="0.0.2",
     license="GPLv3",
     author="Alec Ostrander",
     url="https://github.com/alecglen/cleantimer",
     description="Track progress of long-running scripts, without cluttering your code with log statements.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["tests"]),
     classifiers=[
         "Topic :: Utilities",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent"
+        "Operating System :: OS Independent",
     ],
     keywords=["time", "timer", "progress"],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     py_modules=["cleantimer"],
-    package_dir={'':'cleantimer'},
-    install_requires=["contexttimer", "tqdm", "pandas"]
+    # package_dir={"": "cleantimer"},
+    install_requires=["contexttimer", "tqdm", "pandas"],
 )
